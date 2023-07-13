d1af78968a3f8ab202ee3e78e7318194a7eb70d6
Mohit Varshney <mohit.varshney@perceptknorigin.com>
2023-07-13 18:51:03 +0530

    about section

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("scroll-head");
+  } else {
+    header.classList.remove("scroll-head");
+  }
+}
