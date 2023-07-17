4f016d7a8dba1a7b20676a8d7c483f590512b9fd
Mohit Varshney <mohit.varshney@perceptknorigin.com>
2023-07-17 19:07:22 +0530

    web layout done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,22 @@
-window.onscroll = function() {myFunction()};
 
-var header = document.getElementById("myHeader");
-var sticky = header.offsetTop;
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("scroll-head");
-  } else {
-    header.classList.remove("scroll-head");
-  }
+window.onscroll = () =>{
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 100;
+    let height = sec.offsetHeight;
+    let id  = sec.getAttribute('id');
+
+    if (top >= offset && top < offset + height){
+      navLinks.forEach(links => {
+        links.classList.remove('active');
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+      });
+    }
+
+    let header = document.querySelector('header');
+    header.classList.toggle('scroll-head', window.scrollY > 100)
+  });
 }
