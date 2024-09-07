a04809999cc537e48a0390339717054e25718109
MOHIT VARSHNEY <varshneymohit35@gmail.com>
2024-09-08 00:32:00 +0530

    smtp request

---
+++ phpmailer/send_mail.php
@@ -0,0 +1,53 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Collect form data from AJAX request
+    $name = $_POST['name'];
+    $mobile = $_POST['mobile'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    // Validation
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "Invalid email format";
+        exit;
+    }
+
+    // Create instance of PHPMailer
+    $mail = new PHPMailer(true);
+
+    try {
+        //Server settings
+        $mail->isSMTP();                                            // Set mailer to use SMTP
+        $mail->Host       = 'smtp.gmail.com';                       // Specify main and backup SMTP servers
+        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+        $mail->Username   = 'varshneymohit35@gmail.com';                  // Your Gmail address
+        $mail->Password   = 'Mohit@130422';              // Your Gmail app password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption
+        $mail->Port       = 587;                                    // TCP port to connect to
+
+        //Recipients
+        $mail->setFrom($email, $name);                              // Sender's email and name
+        $mail->addAddress('varshneymohit35@gmail.com');                   // Your Gmail as the recipient
+
+        // Content
+        $mail->isHTML(true);                                        // Set email format to HTML
+        $mail->Subject = 'New Contact Form Submission';
+        $mail->Body    = "You have received a new message from the contact form.<br><br>".
+                         "<b>Name:</b> $name<br>".
+                         "<b>Mobile:</b> $mobile<br>".
+                         "<b>Email:</b> $email<br>".
+                         "<b>Message:</b><br>$message";
+
+        $mail->send();
+        echo 'Message has been sent successfully';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+}
+?>
