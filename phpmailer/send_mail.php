fb82bf346ec863a6029f2dcc68e9d8de85e0ea2a
MOHIT VARSHNEY <varshneymohit35@gmail.com>
2024-09-08 02:07:57 +0530

    smtp request

--- phpmailer/send_mail.php
+++ phpmailer/send_mail.php
@@ -1,6 +1,7 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
 
 require 'vendor/autoload.php';
 
@@ -23,26 +24,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     try {
         //Server settings
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER; 
         $mail->isSMTP();                                            // Set mailer to use SMTP
         $mail->Host       = 'smtp.gmail.com';                       // Specify main and backup SMTP servers
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
         $mail->Username   = 'varshneymohit35@gmail.com';                  // Your Gmail address
-        $mail->Password   = 'Mohit@130422';              // Your Gmail app password
+        $mail->Password   = 'nqcg eabm gtlm bsdp';              // Your Gmail app password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption
         $mail->Port       = 587;                                    // TCP port to connect to
-
+        
         //Recipients
         $mail->setFrom($email, $name);                              // Sender's email and name
         $mail->addAddress('varshneymohit35@gmail.com');                   // Your Gmail as the recipient
 
         // Content
         $mail->isHTML(true);                                        // Set email format to HTML
-        $mail->Subject = 'New Contact Form Submission';
-        $mail->Body    = "You have received a new message from the contact form.<br><br>".
+        $mail->Subject = 'Website Lead';
+        $mail->Body    = "Hi Rahul, You have received new request from you Website. Please take a look on that.<br><br>".
                          "<b>Name:</b> $name<br>".
                          "<b>Mobile:</b> $mobile<br>".
                          "<b>Email:</b> $email<br>".
-                         "<b>Message:</b><br>$message";
+                         "<b>Message:</b> $message";
 
         $mail->send();
         echo 'Message has been sent successfully';
